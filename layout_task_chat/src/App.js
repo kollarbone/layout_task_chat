f4af65db6c0fcd3c1868fac5076f18860619e53c
Polina <106979919+kollarbone@users.noreply.github.com>
2023-02-26 22:47:20 +0300

    snmth

---
+++ layout_task_chat/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="messages"></div>
+      <div className="message_input">
+        <textarea className="textarea" />
+        <bottom className="bottom">
+          <p>Отправить</p>
+        </bottom>
+      </div>
+    </div>
+  );
+}
+
+export default App;
