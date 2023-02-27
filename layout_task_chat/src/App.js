c64d11b6fa9dcc40727a6da85323f5a573e2acf3
Polina <106979919+kollarbone@users.noreply.github.com>
2023-02-27 19:32:19 +0300

    done

--- layout_task_chat/src/App.js
+++ layout_task_chat/src/App.js
@@ -1,9 +1,51 @@
 import "./App.css";
-
+import { Messages } from "./data.js";
 function App() {
   return (
     <div className="App">
-      <div className="messages"></div>
+      <div className="messages">
+        <div className="messages_scroll">
+          {Messages.map((i) => (
+            <>
+              {i.position === 1 && !i.day && (
+                <div className="left-mes">
+                  <img src={i.img} alt="img" />
+                  <p>{i.text}</p>
+                </div>
+              )}
+              {i.position === 2 && !i.day && (
+                <div className="right-mes">
+                  <img src={i.img} alt="img" />
+                  <p>{i.text}</p>
+                </div>
+              )}
+            </>
+          ))}
+          <div className="new-mes">
+            <div className="line">
+              <div></div>
+              <p>21 октября 2017</p>
+              <div></div>
+            </div>
+            {Messages.map((i) => (
+              <>
+                {i.position === 1 && i.day && (
+                  <div className="left-mes">
+                    <img src={i.img} alt="img" />
+                    <p>{i.text}</p>
+                  </div>
+                )}
+                {i.position === 2 && i.day && (
+                  <div className="right-mes">
+                    <img src={i.img} alt="img" />
+                    <p>{i.text}</p>
+                  </div>
+                )}
+              </>
+            ))}
+          </div>
+        </div>
+      </div>
       <div className="message_input">
         <textarea className="textarea" />
         <bottom className="bottom">
